ed1004e2f9278a8511452884afeca21a1ab2ca0a
wes337 <wesmoses@gmail.com>
2025-09-12 22:05:03 +0300

    Revert "animation stuck fix"
    This reverts commit b2a4d7b0d55ac92c87a1399319bef8b3ec368ec2.

--- src/app/page.js
+++ src/app/page.js
@@ -39,10 +39,6 @@ export default function Home() {
     animating.current = true;
 
     setCurrentSlide(index);
-
-    timeout.current = setTimeout(() => {
-      animating.current = false;
-    }, 500);
   }, []);
 
   const gotoNextSlide = useCallback(() => {
@@ -61,10 +57,6 @@ export default function Home() {
 
       return nextSlide;
     });
-
-    timeout.current = setTimeout(() => {
-      animating.current = false;
-    }, 500);
   }, []);
 
   const gotoPreviousSlide = useCallback(() => {
@@ -83,10 +75,6 @@ export default function Home() {
 
       return previousSlide;
     });
-
-    timeout.current = setTimeout(() => {
-      animating.current = false;
-    }, 500);
   }, []);
 
   useGSAP(() => {
@@ -218,6 +206,13 @@ export default function Home() {
               key={slide.name}
               index={index}
               slide={slide}
+              onClick={() => {}}
+              onOpenPhoto={() => {
+                animating.current = true;
+              }}
+              onClosePhoto={() => {
+                animating.current = false;
+              }}
               onLoad={() => setLoaded((loaded) => loaded + 1)}
             />
           );
@@ -287,7 +282,11 @@ function Slide(props) {
   };
 
   return (
-    <div id={`slide-${props.index}`} className="select-none">
+    <div
+      id={`slide-${props.index}`}
+      className="select-none"
+      onClick={props.onClick}
+    >
       <div className="bg fixed top-[50%] left-[50%] w-[66vw] h-[66vh] brightness-60 select-none">
         <Image
           className="w-full h-full object-cover"
