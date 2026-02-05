77e9541c78fa8d4af517fed7133666d13c9f815a
wes337 <wesmoses@gmail.com>
2026-02-05 18:07:42 +0200

    update deps

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1770306774793,
+    __BUILD_TIMESTAMP_MS__: 1770307355450,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
